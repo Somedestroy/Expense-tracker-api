5a8d26bafb1ce0f03d7110483c0457965a624ab0
Фомичёв Максим Дмитриевич <maksim.fomichev@itb.local>
2022-04-05 13:29:44 +0300

    Initial commit

---
+++ src/main/java/com/pairlearning/expensetrackerapi/exceptions/EtResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pairlearning.expensetrackerapi.exceptions;
+
+public class EtResourceNotFoundException extends RuntimeException {
+    public EtResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/pairlearning/expensetrackerapi/services/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.pairlearning.expensetrackerapi.services;
+
+import com.pairlearning.expensetrackerapi.domain.User;
+import com.pairlearning.expensetrackerapi.exceptions.EtAuthException;
+import com.pairlearning.expensetrackerapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User validateUser(String email, String password) throws EtAuthException {
+        if (email != null) email = email.toLowerCase();
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+
+    @Override
+    public User registerUser(String firstName, String lastName, String email, String password) throws EtAuthException {
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if (email != null) email = email.toLowerCase();
+        if (!pattern.matcher(email).matches())
+        {
+            throw new EtAuthException("Invalid email format");
+        }
+        Integer count = userRepository.getCountByEmail(email);
+        if (count > 0)
+        {
+            throw new EtAuthException("This email is already used");
+        }
+        Integer userId = userRepository.create(firstName, lastName, email, password);
+        return userRepository.findById(userId);
+    }
+}
